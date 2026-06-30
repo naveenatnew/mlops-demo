e6607475450ef796c146a549e160291f828390a4
Naveen <naveenatnew@gmail.com>
2026-06-30 11:48:25 +0530

    Fixed black

--- app/api.py
+++ app/api.py
@@ -9,27 +9,30 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 MODEL_PATH = BASE_DIR / "saved_model" / "model.pkl"
 model = joblib.load(MODEL_PATH)
 
+
 class IrisRequest(BaseModel):
     sepal_length: float
     sepal_width: float
     petal_length: float
     petal_width: float
 
+
 @app.get("/")
 def home():
     return {"message": "ML Prediction API is running"}
 
+
 @app.post("/predict")
 def predict(request: IrisRequest):
-    features = [[
-        request.sepal_length,
-        request.sepal_width,
-        request.petal_length,
-        request.petal_width
-    ]]
+    features = [
+        [
+            request.sepal_length,
+            request.sepal_width,
+            request.petal_length,
+            request.petal_width,
+        ]
+    ]
 
     prediction = model.predict(features)
 
-    return {
-        "prediction": int(prediction[0])
-    }
+    return {"prediction": int(prediction[0])}
--- tests/test_api.py
+++ tests/test_api.py
@@ -3,10 +3,9 @@ from app.api import app
 
 client = TestClient(app)
 
+
 def test_home():
     response = client.get("/")
 
     assert response.status_code == 200
-    assert response.json() == {
-        "message": "ML Prediction API is running"
-    }
+    assert response.json() == {"message": "ML Prediction API is running"}
