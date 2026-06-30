7964dcd4a6c2ee9fd745f6e813cf949d9cd40703
Naveen <naveenatnew@gmail.com>
2026-06-30 16:37:42 +0530

    Updates

--- infra.ps1
+++ infra.ps1
@@ -4,6 +4,9 @@ $AKS_NAME = "aks-mlops-demo"
 
 $IDENTITY_NAME = "id-gha-mlops"
 
+$GITHUB_ORG = "naveenatnew"
+$REPO = "mlops-demo"
+
 az group create `
     --name $RESOURCE_GROUP `
     --location $LOCATION
@@ -43,3 +46,28 @@ $CLIENT_ID
 $PRINCIPAL_ID
 $SUBSCRIPTION_ID
 
+$AKS_ID = az aks show `
+    --resource-group $RESOURCE_GROUP `
+    --name $AKS_NAME `
+    --query id `
+    -o tsv
+
+az role assignment create `
+    --assignee-object-id $PRINCIPAL_ID `
+    --assignee-principal-type ServicePrincipal `
+    --role "Azure Kubernetes Service Cluster User Role" `
+    --scope $AKS_ID    
+
+
+$FEDERATED_CREDENTIAL_NAME = "github-main"
+
+$GITHUB_USERNAME = "naveenatnew"
+$REPO_NAME = "mlops-demo"
+
+az identity federated-credential create `
+    --name "github-main" `
+    --identity-name $IDENTITY_NAME `
+    --resource-group $RESOURCE_GROUP `
+    --issuer "https://token.actions.githubusercontent.com" `
+    --subject "repo:$GITHUB_USERNAME/$REPO_NAME:ref:refs/heads/main" `
+    --audiences "api://AzureADTokenExchange"
